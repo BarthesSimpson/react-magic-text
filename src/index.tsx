0579d8b35dba6cf1d66904938fd24cd496e448b1
BarthesSimpson <d.s.frenk@gmail.com>
2019-07-31 18:50:45 -0700

    Add storybook

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+// import * as React from 'react';
+import React from 'react';
+
+function MagicText() {
+  return <div>Magic text</div>;
+}
+
+export default MagicText;
---
+++ stories/index.stories.tsx
@@ -0,0 +1,8 @@
+// import * as React from 'react';
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MagicText from '../src';
+
+const stories = storiesOf('Components', module);
+
+stories.add('MagicText', () => <MagicText />);
